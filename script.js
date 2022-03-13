cb31e7b789ea248eeba9295656210e6f6e7361a4
John Patrick C. Perez <patrickperez527@gmail.com>
2022-03-13 21:52:12 +0800

    added local storage

--- script.js
+++ script.js
@@ -1,99 +1,46 @@
-const actions = Array.from(document.querySelectorAll('[data-action]'));
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
 let counter = localStorage.getItem('counter') || 0;
-let counterValue = document.querySelector('.counter-value');
+const value = document.getElementById('value');
+const title = document.getElementById('title');
+const btns = document.querySelectorAll('.btn');
 
-counterValue.innerText = counter;
+value.textContent = counter;
 
-actions.forEach(action => {
-    action.addEventListener('click', () => {
-        const type = action.dataset.action;
+if (counter > 0) {
+    value.style.color = '#198754';
+} else if (counter < 0) {
+    value.style.color = '#DC3545';
+} else {
+    value.style.color = '#212529';
+}
 
-        switch(type) {
-            case 'increase':
-                counter++;
-                break;
-            case 'decrease':
-                counter--;
-                break;
-            case 'reset':
-                counter = 0;
-                break;
-            case 'save':
-                localStorage.setItem('counter', counter);
-                break; 
-            case 'load':
-                counter = localStorage.getItem('counter');
-                break;
-            case 'clear':
-                localStorage.clear();
-                counter = 0;
-                break;   
+btns.forEach(btn => {
+    btn.addEventListener('click', e => {
+        const style = e.currentTarget.classList;
+
+        if (style.contains('decrease')) {
+            counter--;
+            localStorage.setItem('counter', counter);
+        } else if (style.contains('reset')) {
+            counter = 0;
+            localStorage.setItem('counter', counter);
+        } else {
+            counter++;
+            localStorage.setItem('counter', counter);
         }
 
         if (counter > 0) {
-            counterValue.style.color = '#28a745'
+            value.style.color = '#198754';
         } else if (counter < 0) {
-            counterValue.style.color = '#dc3545'
-        } else if (counter === 0){
-            counterValue.style.color = '#212529'
+            value.style.color = '#DC3545';
+        } else {
+            value.style.color = '#212529';
         }
 
-        counterValue.innerText = counter;
+        value.textContent = counter;
     })
-})
-
-const decreaseBtn = document.getElementById('decrease');
-const resetBtn = document.getElementById('reset');
-const increaseBtn = document.getElementById('increase');
-const saveBtn = document.getElementById('save');
-const loadBtn = document.getElementById('load');
-const clearBtn = document.getElementById('clear');
-
-decreaseBtn.addEventListener('mouseover', () => {
-    decreaseBtn.textContent = 'decrease';
-})
-
-decreaseBtn.addEventListener('mouseout', () => {
-    decreaseBtn.innerHTML = '<i class="fa-solid fa-minus"></i>';
-})
-
-resetBtn.addEventListener('mouseover', () => {
-    resetBtn.textContent = 'reset';
-})
-
-resetBtn.addEventListener('mouseout', () => {
-    resetBtn.innerHTML = '<i class="fa fa-refresh"></i>';
-})
-
-increaseBtn.addEventListener('mouseover', () => {
-    increaseBtn.textContent = 'increase';
-})
-
-increaseBtn.addEventListener('mouseout', () => {
-    increaseBtn.innerHTML = '<i class="fa-solid fa-plus"></i>';
-})
-
-saveBtn.addEventListener('mouseover', () => {
-    saveBtn.textContent = 'save';
-})
-
-saveBtn.addEventListener('mouseout', () => {
-    saveBtn.innerHTML = '<i class="fa-solid fa-floppy-disk"></i>';
-})
-
-loadBtn.addEventListener('mouseover', () => {
-    loadBtn.textContent = 'load';
-})
-
-loadBtn.addEventListener('mouseout', () => {
-    loadBtn.innerHTML = '<i class="fa-solid fa-spinner"></i>';
-})
-
-clearBtn.addEventListener('mouseover', () => {
-    clearBtn.textContent = 'clear';
-})
-
-clearBtn.addEventListener('mouseout', () => {
-    clearBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
 })
